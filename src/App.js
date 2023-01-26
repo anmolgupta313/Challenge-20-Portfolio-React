e7ffe608817133b89b080fdbd107501130ddd03a
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-01-26 18:10:07 -0500

    Created jsx for other pages and also wrote a usestate for rendering different page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+
 
 import Navbar from './component/navbar';
 
 function App() {
+const [currentPage, setCurrentPage]= useState('Home');
+
+function renderAll(){
+if(currentPage==="about"){
+  return 
+}
+if(currentPage==="portfolio"){
+  return
+}
+if(currentPage==="contact"){
+  return
+}
+if(currentPage==="resume"){
+  return
+}
+else{
+  return
+}
+}
+
+const pageChange= (page)=>{
+setCurrentPage(page);
+}
   return (
     <div className="App">
    <div>
-    <Navbar />
+    <Navbar  currentPage= {currentPage}  pageChange={pageChange} />
+
+    {renderAll()}
    </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Navbar(){
+function Navbar(props){
     return(
         <nav className="nav">
             <div className="logo-nav">
@@ -8,10 +8,10 @@ function Navbar(){
             </div>
             <div className="nav-link">
                 <ul>
-                    <div><a href="#"><li>About Me</li></a></div>
-                    <div><a href="#"><li>Portfolio</li></a></div>
-                    <div><a href="#"><li>Contact</li></a></div>
-                    <div><a href="#"><li>Resume</li></a></div>
+                    <div><a href="#" onClick={props.pageChange('about')}><li>About Me</li></a></div>
+                    <div><a href="#" onClick={props.pageChange('portfolio')}><li>Portfolio</li></a></div>
+                    <div><a href="#" onClick={prope.pageChange('contact')}><li>Contact</li></a></div>
+                    <div><a href="#" onClick={props.pageChange('resume')}><li>Resume</li></a></div>
                 </ul>
             </div>
         </nav>
