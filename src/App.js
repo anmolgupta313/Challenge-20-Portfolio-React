196535c60e9db09acc02c9afdb53296f69427175
Anmol Gupta <anmol.gupta2180@gmail.com>
2024-03-29 20:57:40 -0400

    I have deployed the project using gh pages and chnaged the browerouter with hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter,BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Header from "./component/navbar";
@@ -21,7 +21,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header windowDimenssion={windowDimenssion} detectSize={detectSize} />
       <div className="App">
         <Routes>
@@ -33,7 +33,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
